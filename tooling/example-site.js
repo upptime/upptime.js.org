4503c9d24b6d63c0f03b4bc458a1fc4424ebe07e
Anand Chowdhary <mail@anandchowdhary.com>
2020-10-27 16:24:27 +0530

    :sparkles: Add example status site

---
+++ tooling/example-site.js
@@ -0,0 +1,12 @@
+const { execSync } = require("child_process");
+
+execSync(
+  `git clone --single-branch --branch gh-pages https://github.com/upptime/upptime status`,
+  {
+    stdio: "ignore",
+  }
+);
+
+execSync("cp -r status build", {
+  stdio: "ignore",
+});
